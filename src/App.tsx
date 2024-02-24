1ee8eb1c084763df625c6142149fc47343da1236
Suhani-013 <126856854+Suhani-013@users.noreply.github.com>
2024-02-24 22:27:04 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App(): JSX.Element {
+  return (
+    <div className="min-h-screen flex">
+      <div className="m-auto">
+        <h1 className="font-bold text-3xl">Hello Suhani!</h1>
+      </div>
+    </div>
+  );
+}
